d85afdc2284d9b4092bd6a3037cd67bd803546ed
Stark-banana322 <79390634+Stark-banana322@users.noreply.github.com>
2021-07-03 00:48:23 +0400

    Add files via upload

--- Js.js
+++ Js.js
@@ -1,2 +1,12 @@
 console.log(8)
 document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>')
+
+const iconMenu = document.querySelector('.menu_icon')
+if (iconMenu) {
+	const menuBody = document.querySelector('.menu_body')
+	iconMenu.addEventListener("click", function(e){
+		document.body.classList.toggle('_lock')
+		iconMenu.classList.toggle('_active')
+		menuBody.classList.toggle('_active')
+	})
+}
